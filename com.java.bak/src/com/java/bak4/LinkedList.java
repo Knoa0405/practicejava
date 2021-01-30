d09cc4b28c0615ea9e8222853854866002dc17e0
Noah <mike1124@naver.com>
2021-01-30 21:57:01 +0900

    Use LinkedList

--- com.java.bak/src/com/java/bak4/LinkedList.java
+++ com.java.bak/src/com/java/bak4/LinkedList.java
@@ -1,6 +1,7 @@
 package com.java.bak4;
 
 public class LinkedList {
+	
 	private int elements;
 	
 	private LinkedList next = null;
@@ -25,5 +26,75 @@ public class LinkedList {
 		return this;
 	}
 	
-	public LinkedList add(LinkedList head, )
+	public LinkedList add(LinkedList head, LinkedList nodeToAdd, int position) {
+		LinkedList nextNode = head;
+		
+		for (int loop =0; loop < position-1; loop++) {
+			if(nextNode.next == null) {break;}
+			
+			nextNode = nextNode.next;
+		}
+		LinkedList tmp = nextNode.next;
+		nextNode.next = nodeToAdd;
+		nodeToAdd.next = tmp;
+		
+		return this;
+	}
+	
+	public LinkedList remove(LinkedList head, int positionToRemove) {
+		LinkedList nextNode = head;
+		
+		for (int loop = 0; loop < positionToRemove-1; loop++) {
+			nextNode = nextNode.next;
+		}
+		
+		LinkedList tmp = nextNode.next;
+		nextNode.next = tmp.next;
+		tmp = null;
+		
+		return this;
+	}
+	
+	public boolean contains(LinkedList head, LinkedList nodeToCheck) {
+		LinkedList nextNode = head;
+		
+		while(nextNode.next != null) {
+			if(nextNode.elements == nodeToCheck.elements) {
+				return true;
+			}
+			nextNode = nextNode.next;
+		}
+		
+		return false;
+	}
+	
+	public void printForEach() {
+		LinkedList nextNode = this;
+		
+		while(nextNode != null) {
+			System.out.println(nextNode.elements);
+			nextNode = nextNode.next;
+		}
+	}
+	
+	public int size() {
+		LinkedList nextNode = this;
+		
+		int size = 0;
+		while ( nextNode != null ) {
+			++size;
+			nextNode = nextNode.next;
+		}
+		
+		return size;
+	}
+	
+	public static void main(String[] args) {
+		LinkedList Link = new LinkedList(1);
+		LinkedList Link2 = new LinkedList(5);
+		LinkedList Link3 = new LinkedList(10);
+		Link.add(Link2);
+		Link.add(Link3);
+		Link.printForEach();
+	}
 }
